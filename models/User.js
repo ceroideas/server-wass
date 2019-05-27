f0acecc3a8a5102b5f0901a8842824fdc5723c65
Cesar Montenegro <montenegroback@gmail.com>
2019-05-27 17:24:54 -0400

    ADD: Modelo User.js

---
+++ models/User.js
@@ -0,0 +1,38 @@
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  firstName: {
+    type: String,
+  },
+  nationality: {
+    type: String,
+  },
+  avatar: {
+    type: String
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  },
+  updatedOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Users", UserSchema);
