d39c730ba3e6480f4d265c07279bc531b71abee2
Cesar Montenegro <montenegroback@gmail.com>
2019-05-27 15:34:21 -0400

    INIT Project

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const app = express();
+
+/*
+* @ Routes
+*/
+const testRoutes = require('./routes/test');
+
+/***/
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
+app.use(bodyParser.urlencoded({exrended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.use(testRoutes);
+
+app.listen(process.env.PORT || 3000);
