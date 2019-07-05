c54f52209acaef0c906bd28cf3d02f1ad81b8937
Cesar Montenegro <montenegroback@gmail.com>
2019-07-05 12:56:05 -0400

    UPDATE: Server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const webRouters = require('./routes/app_web/index');
 /***/
 
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://dboda.eu/');
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header("Access-Control-Allow-Credentials", true);
 	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
