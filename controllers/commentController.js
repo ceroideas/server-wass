340f4a2c7a5614ca5f1a91eb94ed780a9537b431
Cesar Montenegro <montenegroback@gmail.com>
2019-07-10 17:41:28 -0400

    ADD: Controlador y Modelo de las comentarios

---
+++ controllers/commentController.js
@@ -0,0 +1,30 @@
+const Comment = require('../models/Comment');
+
+module.exports = {
+
+    create: (req, res) => {
+        res.status(201).json({success: true, message: 'comment create'});
+    },
+
+    findAll: (req, res) => {
+        const getComments = Comment.find().exec();
+        
+        getComments.then((comments) => {
+            res.status(201).json({success: true, comments});
+        }).catch((error) => {
+            res.status(500).send({success: false, error});
+        });
+    },
+
+    update: (req, res) => {
+        res.status(201).json({success: true, message: 'comment update'});
+    },
+
+    findOne: (req, res) => {
+        res.status(201).json({success: true, message: 'comment findOne'});
+    },
+
+    delete: (req, res) => {
+        res.status(201).json({success: true, message: 'comment delete'});
+    },
+}
---
+++ models/Comment.js
@@ -0,0 +1,29 @@
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  comment: {
+    type: String
+  },
+
+  location: {
+    type: Object
+  },
+
+  status: {
+    type: String
+  },
+
+  createdOn: {
+    type: Date,
+    default: Date.now
+  },
+  
+  updatedOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
