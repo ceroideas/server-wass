876df54ecf186d922d3709b555a00ffa58c995a6
Jorge Solano <jorgesolano92@gmail.com>
2020-02-13 21:09:49 -0400

    chat

--- models/Message.js
+++ models/Message.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const authorSchema = new Schema({_id: String,firstName:String, avatar:String})
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({  
@@ -15,10 +16,10 @@ const MessageSchema = new Schema({
       type: String,
       required: true
     },
-    author: {
+    author: [authorSchema]/*{
       type: Schema.Types.ObjectId,
-      ref: 'User'
-    }
+      required: true
+    }*/
   },
   {
     timestamps: true
